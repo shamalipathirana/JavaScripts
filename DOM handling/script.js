5ae07cf0ba21dca24602820d3c7e3df180f81048
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-06-27 17:38:32 +0530

    Create DOM folder

---
+++ DOM handling/script.js
@@ -0,0 +1,16 @@
+
+console.dir(document);
+
+//get properties
+
+console.log(document.URL);
+console.log(document.charset);
+console.log(document.lastModified);
+console.log(document.title);
+
+console.log(document.head);
+console.log(document.body);
+
+document.title="Document Object Model";
+console.log(document.title);
+
