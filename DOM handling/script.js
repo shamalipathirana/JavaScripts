b066d014b2b5b6cb392081bfe029a0e9dbba79ed
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-06-27 18:58:02 +0530

    Select elements using id

--- DOM handling/script.js
+++ DOM handling/script.js
@@ -11,6 +11,13 @@ console.log(document.title);
 console.log(document.head);
 console.log(document.body);
 
+//change values
 document.title="Document Object Model";
 console.log(document.title);
 
+//...............................
+
+//-------Select elements using ID
+console.log(document.getElementById('frmt'));
+var fromTitle=document.getElementById('frmt');
+console.log(fromTitle);
